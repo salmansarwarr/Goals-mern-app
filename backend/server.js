7abbdd492bd20144f4a4c3f06870b5bec4d9fefe
salmansarwarr <muhammadsalmansarwar32@gmail.com>
2023-03-27 03:05:11 +0500

    s

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
-const path = require("path");
-const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv").config();
-const colors = require("colors");
-const goalsRouter = require("./src/routes/goals");
-const usersRouter = require("./src/routes/user");
-const { errorHandler } = require("./src/middleware/error");
-const connectDB = require("./src/config/db");
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv').config();
+const colors = require('colors');
+const goalsRouter = require('./src/routes/goals');
+const usersRouter = require('./src/routes/user');
+const { errorHandler } = require('./src/middleware/error');
+const connectDB = require('./src/config/db');
 const port = process.env.PORT || 3000;
 
 const app = express();
@@ -16,23 +16,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/goals", goalsRouter);
-app.use("/api/users", usersRouter);
+app.use('/api/goals', goalsRouter);
+app.use('/api/users', usersRouter);
 
-// Serve frontend
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-    app.get('*', (req, res) =>
-        res.sendFile(
-            path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'), (err) => {
-                res.status(500).send(err);
-            }
-        )
-    );
-} else {
-    app.get("/", (req, res) => res.send("Please set to production"));
-}
+app.get('/', (req, res) => res.send('App is running'));
 
 app.use(errorHandler);
 
